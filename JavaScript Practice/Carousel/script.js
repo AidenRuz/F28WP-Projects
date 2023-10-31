7aa0c3096785c51e8d6157c8f291cdea7f9116a9
AidenRuz <Aiden_Ruz@protonmail.com>
2023-10-31 15:54:26 +0000

    Setup basic structure of a review carousel.

---
+++ JavaScript Practice/Carousel/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Define locations of elements in the DOM
+const reviewerImage = document.getElementById("image");
+const reviewerName = document.getElementById("name");
+const reviewerJob = document.getElementById("job");
+const review = document.getElementById("review")
+const previous= document.getElementById("previous")
+const next = document.getElementById("next")
+const random = document.getElementById("random")
+
+previous.addEventListener('click', function() {
+    
+});
+
+next.addEventListener('click', function() {
+    
+});
+
+random.addEventListener('click', function() {
+    
+});
